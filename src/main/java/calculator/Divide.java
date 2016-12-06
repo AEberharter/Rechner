6f2d9ee215a35c173c2105978b0a0eda8064d200
AEberharter <Alexaeberharter@tsn.at>
2016-12-06 22:11:44 +0100

    Classes & First working vers

--- src/main/java/calculator/Divide.java
+++ src/main/java/calculator/Divide.java
@@ -1,32 +1,13 @@
 package calculator;
-import java.util.Scanner;
 
 public class Divide implements Operator
 {
 	private double n1, n2;
-	private static Scanner scan = new Scanner(System.in);
 	
 	public Divide(double number1, double number2)
 	{
-		try
-		{
-			this.n1 = number1;
-			this.n2 = number2;
-			
-			if(this.n2 == 0)
-			{
-				throw new Exception();
-			}
-		}
-		catch(Exception e)
-		{
-			while(this.n2 == 0)
-			{
-				System.err.println("Division durch 0 nicht erlaubt, bitte einen anderen Divisor angeben!");
-				this.n2 = Parser.toDouble(scan.next());
-			}
-		}
-	
+		this.n1 = number1;
+		this.n2 = number2;
 	}
 
 
